5dcd6740dd66b6fe4637c4b8ee108d945660d01b
ELilyToledo <pl1004491@ahschool.com>
2025-09-04 18:10:04 +0000

    ML juypter first commit

---
+++ Fruit classifier/main.py
@@ -0,0 +1,7 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.lenear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+import pandas as pd
+
+
